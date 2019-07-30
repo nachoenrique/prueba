b6165be3c21debdc3259747e13ac7b1d85fcca12
Nacho <nacho.enrique.97@gmail.com>
2019-07-30 20:07:43 -0300

    Avances platzi

--- src/index.js
+++ src/index.js
@@ -7,18 +7,23 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+import BadgeNew from './pages/BadgeNew'
+import './global.css'
 
-const name = 'Nacho'
+//const name = 'Nacho'
 ////const element = React.createElement('a',{href: 'https://instagram.com'},'Ir a ig.')
+
 const container = document.getElementById('app');
 
 //const jsx = <h1>hola soy {name}</h1>
 
 //const jsx = <div> <h1>hola, soy el mas crack</h1> <p>Soy el nuevo</p>  </div>
-const element = React.createElement(
-'div',
-{},
-React.createElement('h1',{},'hola soy el mas crack.'),
-React.createElement('p',{},'soy el nuevo'))
+//const element = React.createElement(
+//'div',
+//{},
+//React.createElement('h1',{},'hola soy el mas crack.'),
+//React.createElement('p',{},'soy el nuevo'))
+
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<BadgeNew />, container); //¿PARA QUE SIRVE PONER "<Badge />"?
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../componentes/Navbar'
+
+class BadgeNew extends React.Component {
+    render(){
+        return(
+            <div>
+                <Navbar />
+            </div>
+        )
+    }
+}
+
+export default BadgeNew
