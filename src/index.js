6fd827d43b75001b6462906633af7aaf74c4ea0a
eluuugarcia <eluuugarcia@gmail.com>
2019-07-30 14:18:06 -0300

    Cambio de maquina

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//const element = document.createElement('h1');
+//element.innerText = 'Hello, Platzi Badges!';
+
+//const container = document.getElementById('app');
+
+//container.appendChild(element);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = 'Nacho'
+////const element = React.createElement('a',{href: 'https://instagram.com'},'Ir a ig.')
+const container = document.getElementById('app');
+
+//const jsx = <h1>hola soy {name}</h1>
+
+//const jsx = <div> <h1>hola, soy el mas crack</h1> <p>Soy el nuevo</p>  </div>
+const element = React.createElement(
+'div',
+{},
+React.createElement('h1',{},'hola soy el mas crack.'),
+React.createElement('p',{},'soy el nuevo'))
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(element, container);
